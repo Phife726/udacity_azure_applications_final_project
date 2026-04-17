5f5d0f4673a1d1edb27c5854f3a38f1ee5933093
Phife726 <38040293+Phife726@users.noreply.github.com>
2026-04-17 22:57:53 +0000

    Update Identity Invariants: use environment variables for CLIENT_SECRET and CLIENT_ID
    - Replace hardcoded CLIENT_SECRET and CLIENT_ID with os.environ.get() lookups
    - Remove legacy comments referencing KeyVault
    - Simplify MS Authentication section in config.py

--- config.py
+++ config.py
@@ -18,21 +18,14 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
-    ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
-    # In your production app, Microsoft recommends you to use other ways to store your secret,
-    # such as KeyVault, or environment variable as described in Flask's documentation here:
-    # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    # if not CLIENT_SECRET:
-    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+    # Use os.environ.get to pull the values you saved in the Azure Portal
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+    AUTHORITY = "https://login.microsoftonline.com/common"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = os.environ.get('CLIENT_ID')
 
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "/getAToken"
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
